e502b3342383f61fcb685c965819637ad0924134
Bulzay <goawaylol@yandex.ru>
2023-03-05 23:54:07 +0300

    model changes

--- backend/silant/service/models.py
+++ backend/silant/service/models.py
@@ -1,61 +1,42 @@
 from django.db import models
+from directory_choices import *
 
 
-class MachineModel(models.Model):
-    name = models.CharField(max_length=255, unique=True)
+class MachineDirectory(models.Model):
+    type = models.CharField(
+        max_length=3,
+        choices=MACHINE_DIRECTORY_CHOICES,
+        default='NDT',
+    )
+    name = models.CharField(max_length=255)
     description = models.TextField()
 
 
-class Engine(models.Model):
-    name = models.CharField(max_length=255, unique=True)
+class RepairDirectory(models.Model):
+    type = models.CharField(
+        max_length=3,
+        choices=REPAIR_DIRECTORY_CHOICES,
+        default='NDT',
+    )
+    name = models.CharField(max_length=255)
     description = models.TextField()
 
 
-class Transmission(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-    description = models.TextField()
-
-
-class DriveAxle(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-    description = models.TextField()
-
-
-class SteeredAxle(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-    description = models.TextField()
-
-
-class MaintenanceType(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-    description = models.TextField()
-
-
-class MaintenanceHelder(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-    description = models.TextField()
-
-
-class Unit(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-    description = models.TextField()
-
-
-class RecoveryType(models.Model):
-    name = models.CharField(max_length=255, unique=True)
+class ServiceCompany(models.Model):
+    name = models.CharField(max_length=255)
     description = models.TextField()
 
 
 class Machine(models.Model):
     factory_number = models.CharField(max_length=255, unique=True)
-    machine_model = models.ForeignKey(MachineModel, on_delete=models.RESTRICT)
-    engine_model = models.ForeignKey(Engine, on_delete=models.RESTRICT)
+    machine_model = models.ForeignKey(MachineDirectory, on_delete=models.RESTRICT)
+    engine_model = models.ForeignKey(MachineDirectory, on_delete=models.RESTRICT)
     engine_number = models.CharField(max_length=255)
-    transmission_model = models.ForeignKey(Transmission, on_delete=models.RESTRICT)
+    transmission_model = models.ForeignKey(MachineDirectory, on_delete=models.RESTRICT)
     transmission_number = models.CharField(max_length=255)
-    drive_axle_model = models.ForeignKey(DriveAxle, on_delete=models.RESTRICT)
+    drive_axle_model = models.ForeignKey(MachineDirectory, on_delete=models.RESTRICT)
     drive_axle_number = models.CharField(max_length=255)
-    steered_axle_model = models.ForeignKey(SteeredAxle, on_delete=models.RESTRICT)
+    steered_axle_model = models.ForeignKey(MachineDirectory, on_delete=models.RESTRICT)
     steered_axle_number = models.CharField(max_length=255)
     supply_contract_number_date = models.CharField(max_length=255)
     dispatch_date = models.DateField()
@@ -63,28 +44,28 @@ class Machine(models.Model):
     delivery_address = models.TextField()
     equipment = models.TextField()
     client = None
-    service_company = None
+    service_company = models.ForeignKey(ServiceCompany, on_delete=models.RESTRICT)
 
 
 class Maintenance(models.Model):
-    type = models.ForeignKey(MaintenanceType, on_delete=models.RESTRICT)
+    type = models.ForeignKey(RepairDirectory, on_delete=models.RESTRICT)
     date = models.DateField()
     operating_time = models.PositiveIntegerField()
     work_order_number = models.CharField(max_length=255)
     work_order_date = models.DateField()
-    maintenance_helder = models.ForeignKey(MaintenanceHelder, on_delete=models.RESTRICT)
+    maintenance_helder = models.ForeignKey(ServiceCompany, on_delete=models.RESTRICT)
     machine = models.ForeignKey(Machine, on_delete=models.CASCADE)
-    service_company = None
+    service_company = models.ForeignKey(ServiceCompany, on_delete=models.RESTRICT)
 
 
 class Reclamation(models.Model):
     rejection_date = models.DateField()
     operating_time = models.PositiveIntegerField()
-    unit = models.ForeignKey(Unit, on_delete=models.RESTRICT)
+    unit = models.ForeignKey(RepairDirectory, on_delete=models.RESTRICT)
     description = models.TextField()
-    recovery_method = models.ForeignKey(RecoveryType, on_delete=models.RESTRICT)
+    repair_method = models.ForeignKey(RepairDirectory, on_delete=models.RESTRICT)
     spare_parts = models.TextField()
     recovery_date = models.DateField()
     downtime = None
     machine = models.ForeignKey(Machine, on_delete=models.CASCADE)
-    service_company = None
+    service_company = models.ForeignKey(ServiceCompany, on_delete=models.RESTRICT)
