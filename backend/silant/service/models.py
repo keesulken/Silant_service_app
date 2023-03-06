ce5934c3f572606ae263afdea26a4cb8b466fd12
Bulzay <goawaylol@yandex.ru>
2023-03-07 01:59:00 +0300

    more model changes

--- backend/silant/service/models.py
+++ backend/silant/service/models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from choices import *
+from .choices import *
 
 
 class User(AbstractUser):
@@ -34,50 +34,110 @@ class RepairDirectory(models.Model):
     description = models.TextField()
 
 
-class ServiceCompany(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField()
-
-
 class Machine(models.Model):
     factory_number = models.CharField(max_length=255, unique=True)
-    machine_model = models.ForeignKey(MachineDirectory, on_delete=models.RESTRICT)
-    engine_model = models.ForeignKey(MachineDirectory, on_delete=models.RESTRICT)
+    machine_model = models.ForeignKey(
+        MachineDirectory,
+        related_name='machine_models',
+        on_delete=models.RESTRICT
+    )
+    engine_model = models.ForeignKey(
+        MachineDirectory,
+        related_name='engines',
+        on_delete=models.RESTRICT
+    )
     engine_number = models.CharField(max_length=255)
-    transmission_model = models.ForeignKey(MachineDirectory, on_delete=models.RESTRICT)
+    transmission_model = models.ForeignKey(
+        MachineDirectory,
+        related_name='transmissions',
+        on_delete=models.RESTRICT
+    )
     transmission_number = models.CharField(max_length=255)
-    drive_axle_model = models.ForeignKey(MachineDirectory, on_delete=models.RESTRICT)
+    drive_axle_model = models.ForeignKey(
+        MachineDirectory,
+        related_name='drive_axles',
+        on_delete=models.RESTRICT
+    )
     drive_axle_number = models.CharField(max_length=255)
-    steered_axle_model = models.ForeignKey(MachineDirectory, on_delete=models.RESTRICT)
+    steered_axle_model = models.ForeignKey(
+        MachineDirectory,
+        related_name='steered_axles',
+        on_delete=models.RESTRICT
+    )
     steered_axle_number = models.CharField(max_length=255)
     supply_contract_number_date = models.CharField(max_length=255)
     dispatch_date = models.DateField()
     consignee = models.TextField()
     delivery_address = models.TextField()
     equipment = models.TextField()
-    client = None
-    service_company = models.ForeignKey(ServiceCompany, on_delete=models.RESTRICT)
+    client = models.ForeignKey(
+        User,
+        related_name='machines_in_use',
+        on_delete=models.RESTRICT,
+        blank=True
+    )
+    service_company = models.ForeignKey(
+        User,
+        related_name='maintain_units',
+        on_delete=models.RESTRICT,
+        blank=True
+    )
 
 
 class Maintenance(models.Model):
-    type = models.ForeignKey(RepairDirectory, on_delete=models.RESTRICT)
+    type = models.ForeignKey(
+        RepairDirectory,
+        related_name='types',
+        on_delete=models.RESTRICT
+    )
     date = models.DateField()
     operating_time = models.PositiveIntegerField()
     work_order_number = models.CharField(max_length=255)
     work_order_date = models.DateField()
-    maintenance_helder = models.ForeignKey(ServiceCompany, on_delete=models.RESTRICT)
-    machine = models.ForeignKey(Machine, on_delete=models.CASCADE)
-    service_company = models.ForeignKey(ServiceCompany, on_delete=models.RESTRICT)
+    maintenance_holder = models.ForeignKey(
+        RepairDirectory,
+        related_name='holders',
+        on_delete=models.RESTRICT
+    )
+    machine = models.ForeignKey(
+        Machine,
+        related_name='maintenances',
+        on_delete=models.CASCADE
+    )
+    service_company = models.ForeignKey(
+        User,
+        related_name='maintenances',
+        on_delete=models.RESTRICT
+    )
 
 
 class Reclamation(models.Model):
     rejection_date = models.DateField()
     operating_time = models.PositiveIntegerField()
-    unit = models.ForeignKey(RepairDirectory, on_delete=models.RESTRICT)
+    unit = models.ForeignKey(
+        RepairDirectory,
+        related_name='units',
+        on_delete=models.RESTRICT
+    )
     description = models.TextField()
-    repair_method = models.ForeignKey(RepairDirectory, on_delete=models.RESTRICT)
+    repair_method = models.ForeignKey(
+        RepairDirectory,
+        related_name='methods',
+        on_delete=models.RESTRICT
+    )
     spare_parts = models.TextField()
     recovery_date = models.DateField()
-    downtime = None
-    machine = models.ForeignKey(Machine, on_delete=models.CASCADE)
-    service_company = models.ForeignKey(ServiceCompany, on_delete=models.RESTRICT)
+    machine = models.ForeignKey(
+        Machine,
+        related_name='reclamations',
+        on_delete=models.CASCADE
+    )
+    service_company = models.ForeignKey(
+        User,
+        related_name='reclamations',
+        on_delete=models.RESTRICT
+    )
+
+    # @property
+    # def downtime(self):
+    #     return self.recovery_date - self.rejection_date
